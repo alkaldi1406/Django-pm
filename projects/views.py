5bfba21488eac83ac8f9ade5801b4c7ea0322451
alkaldi1406 <al-wafi.2019@hotmail.com>
2024-01-01 18:15:21 +0300

    search and pages

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,4 @@
+import requests
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy, reverse
 from . import models
@@ -6,6 +7,15 @@ from . import forms
 class ProjectListView(ListView):
     model = models.Project
     template_name = 'project/list.html'
+    paginate_by = 6
+
+    def get_queryset(self):
+        query_set = super().get_queryset()
+        where ={}
+        q = self.request.GET.get ('q', None)
+        if q:
+            where['title__icontains'] = q
+        return query_set.filter(**where)
 
 class ProjectCreateView(CreateView):
     model = models.Project
